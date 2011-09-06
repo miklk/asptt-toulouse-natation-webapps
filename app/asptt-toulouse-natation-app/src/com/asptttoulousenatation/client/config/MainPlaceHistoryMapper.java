1616ad243f32761d8028417d964f636b61f81dcc
Michael <mkkmikl@gmail.com>
2011-09-06 20:19:16 +0200

    Remove BasicPlace#null

--- app/asptt-toulouse-natation-app/src/com/asptttoulousenatation/client/config/MainPlaceHistoryMapper.java
+++ app/asptt-toulouse-natation-app/src/com/asptttoulousenatation/client/config/MainPlaceHistoryMapper.java
@@ -1,10 +1,9 @@
 package com.asptttoulousenatation.client.config;
 
-import com.asptttoulousenatation.client.userspace.menu.MenuPlace;
 import com.google.gwt.place.shared.PlaceHistoryMapper;
 import com.google.gwt.place.shared.WithTokenizers;
 
-@WithTokenizers({MenuPlace.Tokenizer.class})
+@WithTokenizers({})
 public interface MainPlaceHistoryMapper extends PlaceHistoryMapper {
 
 }
