4b3cec3e94ddae6b840ef0612e90e27f875629d9
Michael <mkkmikl@gmail.com>
2015-03-15 16:53:36 +0100

    Load parent identifier (principal)

--- app/asptt-toulouse-natation-app/src/com/asptttoulousenatation/core/server/dao/inscription/Inscription2Dao.java
+++ app/asptt-toulouse-natation-app/src/com/asptttoulousenatation/core/server/dao/inscription/Inscription2Dao.java
@@ -1,7 +1,15 @@
 package com.asptttoulousenatation.core.server.dao.inscription;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
 import com.asptttoulousenatation.core.server.dao.DaoBase;
 import com.asptttoulousenatation.core.server.dao.entity.inscription.InscriptionEntity2;
+import com.asptttoulousenatation.core.server.service.EMF;
 
 public class Inscription2Dao extends DaoBase<InscriptionEntity2> {
 
@@ -18,6 +26,20 @@ public class Inscription2Dao extends DaoBase<InscriptionEntity2> {
 	@Override
 	public Object getKey(InscriptionEntity2 pEntity) {
 		return pEntity.getId();
+	}
+	
+	public Collection<Long> getPrincipal() {
+		EntityManager em = EMF.get().createEntityManager();
+		List<Long> lAllList = new ArrayList<Long>();
+		try {
+			TypedQuery<Long> query = em.createQuery("SELECT DISTINCT " + getAlias()
+					+ ".principal FROM " + getEntityClass().getSimpleName() + " " + getAlias(),
+					Long.class);
+			lAllList = query.getResultList();
+		} finally {
+			em.close();
+		}
+		return lAllList;
 	}
 
 }
