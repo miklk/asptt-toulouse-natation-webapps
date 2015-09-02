9091372859ca8307d9bcf3ff13bff015896787a3
Michael <mkkmikl@gmail.com>
2015-09-02 08:39:16 +0200

    Fix URL

--- app/asptt-toulouse-natation-app/war/admin/js/enf/EnfController.js
+++ app/asptt-toulouse-natation-app/war/admin/js/enf/EnfController.js
@@ -4,20 +4,12 @@
 var enfController = angular.module('EnfController', ['ngRoute', 'enfServices', 'groupeServices', 'slotServices']);
 
 enfController.controller('EnfController', ['$rootScope', '$http', '$scope', '$location', '$filter', 'EnfService', 'SlotService', function($rootScope, $http, $scope, $location, $filter, EnfService, SlotService) {
-	$scope.creneauSelected = 0;
-	
+	$rootScope.isLoading = true;
+	$scope.creneauSelected = null;
 	SlotService.all.query({}, function(data) {
 		$scope.creneaux = data.creneaux;
+		$rootScope.isLoading = false;
 	});
-	
-	$scope.jours = function(day, piscine) {
-		$http.get("/resources/enf/jours/" + day + "/" + piscine, {})
-	       .success(function(dataFromServer, status, headers, config) {
-	       })
-	        .error(function(data, status, headers, config) {
-	          alert("Erreur");
-	       });
-	}
 	
 	$scope.creneauLabel = function(creneau) {
 		var label = creneau.swimmingPool + ' - ' + creneau.dayOfWeek + ' - ' + $filter('date') (creneau.beginDt, 'HH:mm') + '-' + $filter('date') (creneau.endDt, 'HH:mm') + ' - (' + (creneau.placeDisponible - creneau.placeRestante) + '/' + creneau.placeDisponible + ')';
@@ -25,14 +17,5 @@ enfController.controller('EnfController', ['$rootScope', '$http', '$scope', '$lo
 			label = label + " #2";
 		}
 		return label;
-	}
-	
-	$scope.presences = function() {
-		$http.get("/resources/enf/presences/" + $scope.creneauSelected, {})
-	       .success(function(dataFromServer, status, headers, config) {
-	       })
-	        .error(function(data, status, headers, config) {
-	          alert("Erreur");
-	       });
 	}
 }]);
