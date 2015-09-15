aa3f922751536ecca96668b8fe3f5fde5d8173f0
Michael <mkkmikl@gmail.com>
2015-09-15 23:46:29 +0200

    E-mail - nouveau envoi d'-mail - affichage du destinataire - possibilité d'envoyer à un destinataire

---
+++ app/asptt-toulouse-natation-app/src/com/asptttoulousenatation/core/adherent/EmailService.java
@@ -0,0 +1,311 @@
+package com.asptttoulousenatation.core.adherent;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Properties;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.mail.Address;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections.Predicate;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.text.StrBuilder;
+import org.glassfish.jersey.media.multipart.FormDataBodyPart;
+import org.glassfish.jersey.media.multipart.FormDataMultiPart;
+
+import com.asptttoulousenatation.core.server.dao.club.group.GroupDao;
+import com.asptttoulousenatation.core.server.dao.club.group.SlotDao;
+import com.asptttoulousenatation.core.server.dao.entity.club.group.GroupEntity;
+import com.asptttoulousenatation.core.server.dao.entity.club.group.SlotEntity;
+import com.asptttoulousenatation.core.server.dao.entity.field.DossierEntityFields;
+import com.asptttoulousenatation.core.server.dao.entity.field.DossierNageurEntityFields;
+import com.asptttoulousenatation.core.server.dao.entity.field.GroupEntityFields;
+import com.asptttoulousenatation.core.server.dao.entity.inscription.DossierEntity;
+import com.asptttoulousenatation.core.server.dao.entity.inscription.DossierNageurEntity;
+import com.asptttoulousenatation.core.server.dao.entity.inscription.DossierStatutEnum;
+import com.asptttoulousenatation.core.server.dao.inscription.DossierDao;
+import com.asptttoulousenatation.core.server.dao.inscription.DossierNageurDao;
+import com.asptttoulousenatation.core.server.dao.search.CriterionDao;
+import com.asptttoulousenatation.core.server.dao.search.Operator;
+
+@Path("/email")
+@Produces("application/json")
+public class EmailService {
+	
+	private static final Logger LOG = Logger.getLogger(EmailService.class
+			.getName());
+
+	private static final int EMAIL_PAQUET = 10;
+	
+	private DossierDao dossierDao = new DossierDao();
+	private DossierNageurDao dao = new DossierNageurDao();
+	private SlotDao slotDao = new SlotDao();
+	private GroupDao groupDao = new GroupDao();
+
+	@POST
+	@Consumes(MediaType.MULTIPART_FORM_DATA)
+	public List<String> email(FormDataMultiPart multiPart) {
+		String destinataireParam = multiPart.getField("destinataire")
+				.getValue();
+		String groupesAsString = multiPart.getField("groupes").getValue();
+		Long creneau = multiPart.getField("creneau").getValueAs(Long.class);
+		String piscine = multiPart.getField("piscine").getValue();
+		String to = multiPart.getField("messageTo").getValue();
+		String from = multiPart.getField("messageFrom").getValue();
+		String subject = multiPart.getField("messageSubject").getValue();
+		String corps = multiPart.getField("messageContent").getValue();
+		FormDataBodyPart fichierMultiPart = multiPart.getField("file");
+		List<String> recipents = new ArrayList<String>();
+		try {
+			Properties props = new Properties();
+			Session session = Session.getDefaultInstance(props, null);
+
+			Set<Long> creneaux = Collections.singleton(creneau);
+			Set<Long> groupes = new HashSet<>();
+			for(String groupeAsString: groupesAsString.split(",")) {
+				groupes.add(Long.valueOf(groupeAsString));
+			}
+			
+			final List<String> destinataires;
+			if (to.contains("@")) {
+				destinataires = Collections.singletonList(to);
+			} else {
+				destinataires = getDestinataires(destinataireParam, groupes, creneaux, piscine);
+			}
+			for (int i = 0; i < destinataires.size(); i += EMAIL_PAQUET) {
+				try {
+					Multipart mp = new MimeMultipart();
+					MimeBodyPart htmlPart = new MimeBodyPart();
+					htmlPart.setContent(corps, "text/html");
+					mp.addBodyPart(htmlPart);
+					MimeMessage msg = new MimeMessage(session);
+					msg.setFrom(new InternetAddress(
+							from,
+							"ASPTT Toulouse Natation"));
+					Address[] replyTo = { new InternetAddress(
+							from,
+							"ASPTT Toulouse Natation") };
+					msg.setReplyTo(replyTo);
+					msg.addRecipient(Message.RecipientType.TO,
+							new InternetAddress(
+									"support@asptt-toulouse-natation.com"));
+					if (fichierMultiPart.getFormDataContentDisposition()
+							.getFileName() != null) {
+						MimeBodyPart attachment = new MimeBodyPart();
+						attachment.setFileName(fichierMultiPart
+								.getFormDataContentDisposition().getFileName());
+						InputStream data = fichierMultiPart
+								.getValueAs(InputStream.class);
+						byte[] fileData = IOUtils.toByteArray(data);
+						String mediaType = fichierMultiPart.getMediaType()
+								.toString();
+						attachment.setContent(
+								new ByteArrayInputStream(fileData), mediaType);
+						mp.addBodyPart(attachment);
+					}
+					msg.setSubject(subject, "UTF-8");
+					msg.setContent(mp);
+
+					int first = i;
+					int end = Math.min(first + EMAIL_PAQUET,
+							destinataires.size());
+					try {
+						for (int j = first; j < end; j++) {
+							String destinataire = destinataires.get(j);
+							if (StringUtils.isNotBlank(destinataire)) {
+								LOG.warning("Send to " + destinataire);
+								msg.addRecipient(Message.RecipientType.BCC,
+										new InternetAddress(destinataire));
+								recipents.add(destinataire);
+							}
+						}
+						Transport.send(msg);
+					} catch (Exception e) {
+						LOG.log(Level.SEVERE, e.getMessage(), e);
+					}
+				} catch (Exception e) {
+					LOG.log(Level.SEVERE, e.getMessage(), e);
+				}
+			}
+			Multipart mp = new MimeMultipart();
+			MimeBodyPart htmlPart = new MimeBodyPart();
+			StrBuilder rapport = new StrBuilder();
+			rapport.appendWithSeparators(recipents, "<br />");
+			htmlPart.setContent("E-mail envoyé à " + new Date() + "<br />"
+					+ rapport.toString(), "text/html");
+			mp.addBodyPart(htmlPart);
+			MimeMessage msg = new MimeMessage(session);
+			msg.setFrom(new InternetAddress(
+					"webmaster@asptt-toulouse-natation.com",
+					"ASPTT Toulouse Natation"));
+			msg.addRecipient(Message.RecipientType.TO, new InternetAddress(
+					"contact@asptt-toulouse-natation.com"));
+			msg.addRecipient(Message.RecipientType.CC, new InternetAddress(
+					"support@asptt-toulouse-natation.com"));
+			msg.setSubject("Rapport d'envoi d'e-mail", "UTF-8");
+			msg.setContent(mp);
+			Transport.send(msg);
+
+		} catch (AddressException e) {
+			LOG.log(Level.SEVERE, e.getMessage(), e);
+
+		} catch (MessagingException e) {
+			LOG.log(Level.SEVERE, e.getMessage(), e);
+		} catch (UnsupportedEncodingException e) {
+			LOG.log(Level.SEVERE, e.getMessage(), e);
+		}
+
+		return recipents;
+	}
+	
+	private List<String> getDestinataires(String destinataire, Set<Long> groupes,
+			Set<Long> creneaux, String piscine) {
+		List<String> destinataires = new ArrayList<>();
+		switch (destinataire) {
+		case "all": {
+			List<CriterionDao<? extends Object>> criteria = new ArrayList<CriterionDao<? extends Object>>(
+					1);
+			criteria.add(new CriterionDao<String>(
+					DossierEntityFields.STATUT, DossierStatutEnum.INSCRIT.name(),
+					Operator.EQUAL));
+			List<DossierEntity> dossiers = dossierDao.find(criteria);
+			for (DossierEntity dossier : dossiers) {
+				fillDestinataires(destinataires, dossier);
+			}
+		};
+			break;
+		case "enf": {
+			List<CriterionDao<? extends Object>> criteria = new ArrayList<CriterionDao<? extends Object>>(
+					1);
+			criteria.add(new CriterionDao<Boolean>(GroupEntityFields.ENF,
+					Boolean.TRUE, Operator.EQUAL));
+			List<GroupEntity> groupeEntities = groupDao.find(criteria);
+			Set<Long> groupesId = new HashSet<>();
+			for (GroupEntity groupeEntity : groupeEntities) {
+				groupesId.add(groupeEntity.getId());
+			}
+			destinataires.addAll(getDestinatairesByGroupe(groupesId,
+					new HashSet<Long>()));
+		};
+			break;
+		case "competiteur": {
+			List<CriterionDao<? extends Object>> criteria = new ArrayList<CriterionDao<? extends Object>>(
+					2);
+			criteria.add(new CriterionDao<Boolean>(GroupEntityFields.COMPETITION,
+					Boolean.TRUE, Operator.EQUAL));
+			criteria.add(new CriterionDao<Boolean>(
+					GroupEntityFields.LICENCEFFN, Boolean.TRUE, Operator.EQUAL));
+			List<GroupEntity> groupeEntities = groupDao.find(criteria);
+			Set<Long> groupesId = new HashSet<>();
+			for (GroupEntity groupeEntity : groupeEntities) {
+				groupesId.add(groupeEntity.getId());
+			}
+			destinataires.addAll(getDestinatairesByGroupe(groupesId,
+					new HashSet<Long>()));
+		};
+			break;
+		case "piscine": {
+			Set<Long> creneauFromPiscine = AdherentBeanTransformer
+					.getInstance().getCreneauIds(piscine);
+			Set<Long> groupePiscineEntities = new HashSet<>();
+			for (Long creneauId : creneauFromPiscine) {
+				SlotEntity slotEntity = slotDao.get(creneauId);
+				groupePiscineEntities.add(slotEntity.getGroup());
+			}
+			destinataires.addAll(getDestinatairesByGroupe(groupePiscineEntities,
+					creneauFromPiscine));
+		};
+			break;
+		case "groupes": {
+			destinataires.addAll(getDestinatairesByGroupe(groupes,
+					new HashSet<Long>()));
+		};
+			break;
+		case "creneau": {
+			destinataires.addAll(getDestinatairesByGroupe(groupes,
+					creneaux));
+		};
+		break;
+		default:
+			LOG.severe(destinataire + " is not handle");
+		}
+		return destinataires;
+	}
+
+	private Set<String> getDestinatairesByGroupe(Set<Long> groupes,
+			final Set<Long> creneaux) {
+		Set<String> destinataires = new HashSet<>();
+		List<CriterionDao<? extends Object>> criteria = new ArrayList<CriterionDao<? extends Object>>(
+				groupes.size());
+		for (Long groupe : groupes) {
+			criteria.add(new CriterionDao<Long>(
+					DossierNageurEntityFields.GROUPE, groupe,
+					Operator.EQUAL));
+		}
+		List<DossierNageurEntity> nageurs = dao.find(criteria, Operator.OR,
+				null);
+		@SuppressWarnings("unchecked")
+		Collection<DossierNageurEntity> nageursSelected = CollectionUtils
+				.select(nageurs, new Predicate() {
+
+					@Override
+					public boolean evaluate(Object pArg0) {
+						DossierNageurEntity nageur = (DossierNageurEntity) pArg0;
+						final boolean valid;
+						if (CollectionUtils.isNotEmpty(creneaux)) {
+							boolean found = false;
+							Iterator<Long> it = creneaux.iterator();
+							while (it.hasNext() && !found) {
+								found = StringUtils.contains(nageur
+										.getCreneaux(), it.next().toString());
+							}
+							valid = found;
+						} else {
+							valid = true;
+						}
+						return valid;
+					}
+				});
+		for (DossierNageurEntity nageur : nageursSelected) {
+			DossierEntity dossier = dossierDao.get(nageur.getDossier());
+			fillDestinataires(destinataires, dossier);
+		}
+		return destinataires;
+	}
+	
+	private void fillDestinataires(Collection<String> destinataires, DossierEntity dossier) {
+		if (StringUtils.isNotBlank(dossier.getEmail())) {
+			destinataires.add(dossier.getEmail());
+		}
+		if(StringUtils.isNotBlank(dossier.getEmailsecondaire())) {
+			destinataires.add(dossier.getEmailsecondaire());
+		}
+	}
+}
--- app/asptt-toulouse-natation-app/war/admin/js/adherent/AdherentEmailController.js
+++ app/asptt-toulouse-natation-app/war/admin/js/adherent/AdherentEmailController.js
@@ -2,7 +2,7 @@
  * 
  */
 var adherentController = angular.module('AdherentEmailCtrl', ['ngRoute', 'textAngular', 'adherentsServices', 'groupeServices', 'slotServices', 'piscineServices']);
-adherentController.controller('AdherentEmailCtrl', ['$http', '$scope', '$location', 'AdherentsService', 'GroupeService', 'SlotService', 'PiscineService', function($http, $scope, $location, AdherentsService, GroupeService, SlotService, PiscineService) {
+adherentController.controller('AdherentEmailCtrl', ['$http', '$scope', '$location', '$filter', 'AdherentsService', 'GroupeService', 'SlotService', 'PiscineService', function($http, $scope, $location, $filter, AdherentsService, GroupeService, SlotService, PiscineService) {
 	$scope.fichier = null;
 	$scope.sender = "contact@asptt-toulouse-natation.com";
 	$scope.subject = "";
@@ -11,6 +11,7 @@ adherentController.controller('AdherentEmailCtrl', ['$http', '$scope', '$locatio
 	$scope.groupesSelected = 0;
 	$scope.creneau = 0;
 	$scope.piscine = "";
+	$scope.recipient = "";
 	
 	
 	$scope.htmlcontent = "<img src=\"https://lh3.googleusercontent.com/-G9O-07NDcNY/VBvelTpt3lI/AAAAAAAABgI/yAYJInY7jU4/w917-h69-no/logo_entete.png\" /><p>Madame, Monsieur,</p>";
@@ -26,10 +27,13 @@ adherentController.controller('AdherentEmailCtrl', ['$http', '$scope', '$locatio
 		$scope.piscines = data.piscines;
 	});
 	$scope.loadCreneau = function() {
-		SlotService.get({groupe: $scope.groupesSelected}, function(data) {
+		SlotService.list.query({groupe: $scope.groupesSelected}, function(data) {
 			$scope.creneaux = data.creneaux;
 			angular.forEach($scope.creneaux, function(creneau) {
-				creneau.label = creneau.dayOfWeek + ' - ' + creneau.swimmingPool + ' - ' + creneau.beginStr + '-' + creneau.endStr; 
+				creneau.label = creneau.swimmingPool + ' - ' + creneau.dayOfWeek + ' - ' + $filter('date') (creneau.beginDt, 'HH:mm') + '-' + $filter('date') (creneau.endDt, 'HH:mm') + ' - (' + (creneau.placeDisponible - creneau.placeRestante) + '/' + creneau.placeDisponible + ')';
+				if(creneau.second) {
+					creneau.label = creneau.label + " #2";
+				}
 			});
 			$scope.showCreneau = true;
 		});
@@ -38,6 +42,7 @@ adherentController.controller('AdherentEmailCtrl', ['$http', '$scope', '$locatio
 	$scope.sendEmail = function() {
 		var formData = new FormData();
 		formData.append("file", $scope.fichier);
+		formData.append("messageTo", $scope.recipient);
 		formData.append("messageFrom", $scope.sender);
 		formData.append("messageSubject", $scope.subject);
 		formData.append("messageContent", $scope.htmlcontent);
@@ -45,7 +50,7 @@ adherentController.controller('AdherentEmailCtrl', ['$http', '$scope', '$locatio
 		formData.append("groupes", $scope.groupesSelected);
 		formData.append("creneau", $scope.creneau);
 		formData.append("piscine", $scope.piscine);
-		$http.post("/resources/adherents/email", formData, {
+		$http.post("/resources/email", formData, {
             transformRequest: angular.identity,
             headers: {'Content-Type': undefined}
         }).
@@ -69,4 +74,21 @@ adherentController.controller('AdherentEmailCtrl', ['$http', '$scope', '$locatio
 		$scope.fichier = file;
 	}
 	
+	$scope.getRecipient = function() {
+		switch($scope.destinataire) {
+		case "all": $scope.recipient = "Tous les adhérents inscrits";
+		break;
+		case "enf": $scope.recipient = "Tous les adhérents des groupes ENF";
+		break;
+		case "competiteur": $scope.recipient = "Tous les adhérents des groupes compétitions";
+		break;
+		case "piscine": $scope.recipient = "Tous les adhérents ayant un créneau à la piscine sélectionnée";
+		break;
+		case "groupes": $scope.recipient = "Tous les adhérents appartenant au(x) groupe(s) sélectionné(s)";
+		break;
+		case "creneau": $scope.recipient = "Tous les adhérents ayant un créneau correspondant à celui sélectionné";
+		break;
+		default: $scope.recipient = "";
+		}
+	}	
 }]);
