459c4129b9523db69487b78456b6db493cb27d1a
Michael <mkkmikl@gmail.com>
2015-09-22 22:38:20 +0200

    Security - init spring security

---
+++ app/asptt-toulouse-natation-app/src/com/asptttoulousenatation/core/spring/SpringSecurityInitializer.java
@@ -0,0 +1,7 @@
+package com.asptttoulousenatation.core.spring;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
--- app/asptt-toulouse-natation-app/src/com/asptttoulousenatation/core/spring/WebSecurityConfig.java
+++ app/asptt-toulouse-natation-app/src/com/asptttoulousenatation/core/spring/WebSecurityConfig.java
@@ -1,33 +1,35 @@
 package com.asptttoulousenatation.core.spring;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
-import org.springframework.web.bind.annotation.RestController;
 
 @Configuration
-@ComponentScan
-@EnableAutoConfiguration
-@RestController
-public class WebSecurityConfig {
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Configuration
-	@EnableWebMvcSecurity
-	protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-		@Override
-		protected void configure(HttpSecurity http) throws Exception {
-			http.authorizeRequests().antMatchers("/toto").permitAll().anyRequest().authenticated().and().formLogin()
-					.loginPage("/#/page/login").permitAll().and().logout().permitAll();
-		}
-
-		@Override
-		protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-			auth.inMemoryAuthentication().withUser("admin").password("admin").roles("ADMIN");
-		}
-
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+	  auth.inMemoryAuthentication().withUser("admin").password("admin").roles("ADMIN");
+	  auth.inMemoryAuthentication().withUser("user").password("user").roles("USER");
 	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		//http.authorizeRequests()
+			//.antMatchers("/admin/**").hasRole("ADMIN")
+			//.and().httpBasic();
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/_ah/**");
+		web.ignoring().antMatchers("/resources/**");
+		web.ignoring().antMatchers("/asptt_toulouse_natation_app/**");
+	}
+
 }
